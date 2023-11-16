4d8bada7449589995ae3bf7fbb46326e2dcfebd0
AnisFetoui <anis.fetoui@esprit.tn>
2023-11-16 11:21:59 +0100

    version anis

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function index(): Response
+    {
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+        ]);
+    }
+}
