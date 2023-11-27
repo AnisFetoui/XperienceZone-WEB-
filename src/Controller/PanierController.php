7a6f305b513b79eafc8f2cfb84159bd490c3a960
ImenHadj <imen.hadjyahya@esprit.tn>
2023-11-27 12:26:14 +0100

    imen

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -10,6 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormEvents;
+use MercurySeries\FlashyBundle\FlashyNotifier;
+
+
 
 #[Route('/panier')]
 class PanierController extends AbstractController
@@ -20,25 +24,55 @@ class PanierController extends AbstractController
         return $this->render('panier/index.html.twig', [
             'paniers' => $panierRepository->findAll(),
         ]);
-    }
+    } 
 
     #[Route('/new', name: 'app_panier_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FlashyNotifier $flashy): Response
     {
         $panier = new Panier();
         $form = $this->createForm(PanierType::class, $panier);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            $produit = $panier->getProduit();
+    
+            // Vérifier si la quantité demandée est disponible en stock
+            if ($produit->getQuantite() < $panier->getQuantitePanier()) {
+                $this->addFlash('mercuryseries_flashy_notification', 'La quantité demandée n\'est pas disponible en stock.');
+                return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+            }
+    
+            // Continuer avec le reste du traitement si la quantité est disponible
+            $prixProduit = $produit->getPrixProd();
+            $quantitePanier = $panier->getQuantitePanier();
+            $total = $prixProduit * $quantitePanier;
+    
+            // Appliquer une remise de 10% si le total est supérieur à 100
+            if ($total > 100) {
+                $remise = $total * 0.10; // 10% de remise
+                $totalAvecRemise = $total - $remise;
+                $panier->setTotal($totalAvecRemise);
+                $this->addFlash('mercuryseries_flashy_notification', 'Vous avez reçu une remise de 10% !');
+            } else {
+                $panier->setTotal($total);
+            }
+    
+            // Mettre à jour le stock du produit
+            $nouvelleQuantite = $produit->getQuantite() - $quantitePanier;
+            $produit->setQuantite($nouvelleQuantite);
+    
             $entityManager->persist($panier);
+            $entityManager->persist($produit); // Mettre à jour le stock du produit
             $entityManager->flush();
-
+            
+            $this->addFlash('mercuryseries_flashy_notification', 'Produit ajouté avec succès :)');
+    
             return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
         }
-
+    
         return $this->renderForm('panier/new.html.twig', [
             'panier' => $panier,
-            'form' => $form,
+            'form' => $form
         ]);
     }
 
@@ -55,18 +89,35 @@ class PanierController extends AbstractController
     {
         $form = $this->createForm(PanierType::class, $panier);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            if ($panier->getQuantitePanier() <= 0) {
+                $this->addFlash('mercuryseries_flashy_notification', 'Quantity must be greater than zero.');
+                return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
+            }
+            // ...
+    
+            // Ajuster le total en fonction de la quantité et du prix du produit
+            $produit = $panier->getProduit();
+            $prixProduit = $produit->getPrixProd();
+            $quantitePanier = $panier->getQuantitePanier();
+            $panier->setTotal($prixProduit * $quantitePanier);
+    
             $entityManager->flush();
+            $this->addFlash('mercuryseries_flashy_notification', 'Product updated successfully :)');
 
             return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->renderForm('panier/edit.html.twig', [
+    
+        return $this->render('panier/edit.html.twig', [
             'panier' => $panier,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
+
+    
+    
+    
 
     #[Route('/{idPanier}', name: 'app_panier_delete', methods: ['POST'])]
     public function delete(Request $request, Panier $panier, EntityManagerInterface $entityManager): Response
@@ -78,6 +129,34 @@ class PanierController extends AbstractController
 
         return $this->redirectToRoute('app_panier_index', [], Response::HTTP_SEE_OTHER);
     }
+   
+   // ...
 
-    
+public function indexx(PanierRepository $panierRepository, EntityManagerInterface $entityManager): Response
+{
+    $panierItems = $panierRepository->findAll();
+
+    $totalPanier = 0.0;
+
+    foreach ($panierItems as $item) {
+        $produit = $item->getProduit();
+
+        if ($produit instanceof Produit) {
+            $prixProduit = $produit->getPrixProd();
+            $quantitePanier = $item->getQuantite();
+            $totalPanier += $prixProduit * $quantitePanier;
+        }
+    }
+
+    return $this->render('panier/index.html.twig', [
+        'panierItems' => $panierItems,
+        'totalPanier' => $totalPanier,
+    ]);
 }
+
+
+
+
+}
+    
+
--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -10,87 +10,70 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use function Symfony\Component\String\u;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-/**#[Route('/produit')]
-class ProduitController extends AbstractController
-{
-    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
-    public function index(ProduitRepository $produitRepository): Response
-    {
-        return $this->render('produit/index.html.twig', [
-            'produits' => $produitRepository->findAll(),
-        ]);
-    }
 
-    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $produit = new Produit();
-        $form = $this->createForm(ProduitType::class, $produit);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($produit);
-            $entityManager->flush();
 
-            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('produit/new.html.twig', [
-            'produit' => $produit,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{idProd}', name: 'app_produit_show', methods: ['GET'])]
-    public function show(Produit $produit): Response
-    {
-        return $this->render('produit/show.html.twig', [
-            'produit' => $produit,
-        ]);
-    }
-
-    #[Route('/{idProd}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(ProduitType::class, $produit);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('produit/edit.html.twig', [
-            'produit' => $produit,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{idProd}', name: 'app_produit_delete', methods: ['POST'])]
-    public function delete(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$produit->getIdProd(), $request->request->get('_token'))) {
-            $entityManager->remove($produit);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
-    }
-}
-*/
 #[Route('/produit')]
 class ProduitController extends AbstractController
+
 {
+
     #[Route('/', name: 'app_produit_index', methods: ['GET'])]
-    public function index(ProduitRepository $produitRepository): Response
+    public function index(Request $request, ProduitRepository $produitRepository, PaginatorInterface $paginator): Response
     {
+        $nomProd = $request->query->get('nomProd');
+        $prixProd = $request->query->get('prixProd');
+    
+        if ($nomProd || $prixProd) {
+            $produitsQuery = $produitRepository->findProductByCriteria($nomProd, $prixProd);
+        } else {
+            $produitsQuery = $produitRepository->createQueryBuilder('p')->getQuery();
+        }
+    
+        $produits = $paginator->paginate(
+            $produitsQuery,
+            $request->query->getInt('page', 1),
+            5 // Nombre d'articles par page
+        );
+    
         return $this->render('produit/index.html.twig', [
-            'produits' => $produitRepository->findAll(),
+            'produits' => $produits,
+            'nomProd' => $nomProd,
+            'prixProd' => $prixProd,
         ]);
     }
 
+    #[Route('/back', name: 'produitback_index', methods: ['GET'])]
+public function backofficeprod(Request $request,ProduitRepository $produitRepository,PaginatorInterface $paginator): Response
+{
+$produitsQuery = $produitRepository->createQueryBuilder('p')->getQuery();
+
+
+    $produits = $paginator->paginate(
+        $produitsQuery,
+        $request->query->getInt('page', 1),
+        5 // Nombre d'articles par page
+    );
+
+    return $this->render('produit/indexadmin.html.twig', [
+        'produits' => $produits,
+    ]);
+}
+
+    #[Route('/back/search', name: 'app_produit_search')]
+    public function searchPage(): Response
+    {
+        return $this->render('produit/search.html.twig');
+    }
+
+   
     #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
 public function new(Request $request, EntityManagerInterface $entityManager): Response
 {
@@ -122,15 +105,13 @@ public function new(Request $request, EntityManagerInterface $entityManager): Re
     ]);
 }
 
-    
-        
-    
-
     #[Route('/{idProd}', name: 'app_produit_show', methods: ['GET'])]
     public function show(Produit $produit): Response
-    {
+    {$isNoteProdDefined = $produit->getNoteProd() !== null;
+
         return $this->render('produit/show.html.twig', [
             'produit' => $produit,
+            'isNoteProdDefined' => $isNoteProdDefined,
         ]);
     }
 
@@ -162,4 +143,60 @@ public function new(Request $request, EntityManagerInterface $entityManager): Re
 
         return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
     }
+   
+   
+    #[Route('/produit/{idProd}/rate/{note}', name: 'rate_product', methods: ['GET'])]
+public function rateProduct(Produit $produit, int $note, EntityManagerInterface $entityManager): RedirectResponse
+{
+    // Validez la note pour vous assurer qu'elle est dans une plage valide (par exemple, 1 à 5)
+    if ($note < 1 || $note > 5) {
+        $this->addFlash('error', 'La note doit être comprise entre 1 et 5.');
+    } else {
+        // Mettez à jour la propriété noteProd du produit
+        $produit->setNoteProd($note);
+
+        // Enregistrez les modifications dans la base de données
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La note a été enregistrée avec succès.');
+    }
+
+    // Redirigez l'utilisateur vers la page du produit
+    return $this->redirectToRoute('app_produit_show', ['idProd' => $produit->getIdProd()]);
 }
+
+#[Route('/details/{idProd}', name: 'app_produits_details', methods: ['GET'])]
+    public function details(string $idProd, ProduitRepository $produitrepository): Response
+    {
+         $produit = $produitrepository->find($idProd);
+    
+        return $this->render('produit/details.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+    #[Route('/search', name: 'ajax_search', methods: ['GET'])]
+    public function search(Request $request, ProduitRepository $produitRepository): JsonResponse
+    {    
+
+        $searchString = $request->query->get('q');
+        $produits = $produitRepository->findProduitByNom($searchString);
+    
+        $produitnom = [];
+        foreach ($produits as $produit) {
+            $produitnom[] = [
+                'idProd' => $produit->getIdProd(),
+                'nomProd' => $produit->getNomProd(),
+            ];
+        }
+    
+        return new JsonResponse(['produits' => $produitnom]);
+    }
+    
+
+}
+
+    
+
+
+    
+    
